f035288f6cc57c0fd92f6874af55d641cbea289c
KC Ukeka <54153295+KCUkeka@users.noreply.github.com>
2024-01-19 15:08:39 -0600

    vsdev

--- baking/views.py
+++ baking/views.py
@@ -5,3 +5,17 @@ from django.http import HttpResponse
 
 def welcome (request):
     return render (request, 'welcome.html')
+
+
+# this is for my baseurl and home templates
+""" def baseurl (request):
+    return render (request, 'baseurl.html')
+
+def home (request):
+    return render (request, 'home.html') """
+
+
+#  getting a main page url
+#  def index(request):
+#     baseurl = request.build_absolue_uri()
+#     return render_to_response('your-template.html'), { 'baseurl' : baseurl } 
